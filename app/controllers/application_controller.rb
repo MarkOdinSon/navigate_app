4f4feaae3928fd1bec1d66182734e3cb285b0121
MarkOdinSon <gladkov.mark.123232@gmail.com>
2022-09-07 17:13:43 +0300

    minor changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  include Pundit
+  before_action :authenticate_user!, :except => ["/users/invitation/accept"]
+  include Pundit::Authorization
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
