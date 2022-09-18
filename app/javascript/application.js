3a0989008605a061807061b0c5511d200c72fcd1
MarkOdinSon <gladkov.mark.123232@gmail.com>
2022-09-18 12:54:12 +0300

    trix_done_but_no_design

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,9 @@
 // Entry point for the build script in your package.json.
-import "@hotwired/turbo-rails"
 
-// Stimulus controllers.
-import "./controllers"
+import "@hotwired/turbo-rails"
 import "trix"
 import "@rails/actiontext"
+import "./controllers"
+
+import * as ActiveStorage from "@rails/activestorage"
+ActiveStorage.start()
